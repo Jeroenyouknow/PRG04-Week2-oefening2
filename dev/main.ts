cbcab2f737b41e03f817436c81709262e6d9d6c7
Jeroen Huiskes <jeroen-h@hotmail.nl>
2017-05-10 11:09:49 +0200

    Working version

--- dev/main.ts
+++ dev/main.ts
@@ -4,20 +4,11 @@
 class Game {
     
     constructor() {
-        // fish
-        let f = document.createElement("fish");
-        document.body.appendChild(f);
 
-        let fishx:number = Math.random() * window.innerWidth;
-        let fishy:number = Math.random() * window.innerHeight;
-
-        // we gebruiken hier left top omdat translate al in de animatie wordt gebruikt                
-        f.style.left = fishx + "px";
-        f.style.top = fishy + "px";
-
-        // de kleur moet random worden
-        f.style.webkitFilter = "hue-rotate(45deg)";
-        f.style.filter = "hue-rotate(45deg)";
+        for(var i = 0; i <100; i++){
+            let fish = new Fish();
+        }
+       
 
         // bubble
         let b = document.createElement("bubble");
